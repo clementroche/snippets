9c9c216c402e92b8c42d9f55d2531093751fe38e
clementroche <rchclement@gmail.com>
2020-12-11 22:25:31 +0100

    tabs

--- glsl/rgb_shift.glsl
+++ glsl/rgb_shift.glsl
@@ -2,13 +2,13 @@
 
 vec4 RGBShift(sampler2D map, vec2 uv, vec2 offset, float power, float barrelness) {
     vec2 center = uv - vec2( .5 );
-	float d = power * length( center );
-	normalize( center );
-	vec2 value = mix( offset * power * 0.1, d * center * offset, barrelness);
+    float d = power * length( center );
+    normalize( center );
+    vec2 value = mix( offset * power * 0.1, d * center * offset, barrelness);
     
     vec4 c1 = texture2D( map, uv - value );
-	vec4 c2 = texture2D( map, uv );
-	vec4 c3 = texture2D( map, uv + value );
+    vec4 c2 = texture2D( map, uv );
+    vec4 c3 = texture2D( map, uv + value );
     
     return vec4( c1.r, c2.g, c3.b, (c1.a + c2.a + c3.a) / 3. );
 }
